c2c668fc5703a2265215b99d8444abe2897abf41
Henrik VT <commits@henrikvt.com>
2026-04-15 19:42:19 -0400

    monitor1 code cleanup

--- machines/monitor1/default.nix
+++ machines/monitor1/default.nix
@@ -12,17 +12,13 @@
     device = "/dev/sda";
   };
 
-  systemd.network = {
-    links = {
-      "10-mgmt-nic0" = {
-        matchConfig = {
-          MACAddress = "bc:24:11:0c:a1:c3";
-          Type = "ether";
-        };
-        linkConfig = {
-          Name = "nic0";
-        };
-      };
+  systemd.network.links."10-mgmt-nic0" = {
+    matchConfig = {
+      MACAddress = "bc:24:11:0c:a1:c3";
+      Type = "ether";
+    };
+    linkConfig = {
+      Name = "nic0";
     };
   };
 }

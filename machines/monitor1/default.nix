fb8b15a52505ed7ea9325df944198d958abfe0e9
Henrik VT <commits@henrikvt.com>
2026-04-15 13:58:54 -0400

    monitor1 node

---
+++ machines/monitor1/default.nix
@@ -0,0 +1,28 @@
+{...}: {
+  imports = [
+    ./hwconfig.nix
+  ];
+
+  networking = {
+    hostName = "monitor1-sbtnvt";
+  };
+
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+  };
+
+  systemd.network = {
+    links = {
+      "10-mgmt-nic0" = {
+        matchConfig = {
+          MACAddress = "bc:24:11:0c:a1:c3";
+          Type = "ether";
+        };
+        linkConfig = {
+          Name = "nic0";
+        };
+      };
+    };
+  };
+}

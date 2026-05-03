b05533243fc43256cedc6135a0b4ba8f62ecc78a
Henrik VT <commits@henrikvt.com>
2026-05-03 14:05:05 -0400

    initialize machine for akvorado

---
+++ machines/akvorado/default.nix
@@ -0,0 +1,24 @@
+{}: {
+  imports = [
+    ./hwconfig.nix
+  ];
+
+  networking = {
+    hostName = "akvorado";
+  };
+
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+  };
+
+  systemd.network.links."10-mgmt-nic0" = {
+    matchConfig = {
+      MACAddress = "bc:24:11:66:22:94";
+      Type = "ether";
+    };
+    linkConfig = {
+      Name = "nic0";
+    };
+  };
+}
